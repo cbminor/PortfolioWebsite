f6cb6be3310e0df3c8db20074f0cd5b9bb1bdd3f
cbminor <chelsieminor@gmail.com>
2025-07-11 12:26:59 -0600

    Add CSRF_TRUSTED_ORIGINS

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -11,6 +11,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "*").split(",")
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
